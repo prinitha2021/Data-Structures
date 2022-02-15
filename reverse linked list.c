96273e4a642145ae3e41233a0b2ac4fb8591d301
prinitha2021 <69192380+prinitha2021@users.noreply.github.com>
2022-02-15 22:47:07 +0530

    Update reverse linked list.c

--- reverse linked list.c
+++ reverse linked list.c
@@ -1,21 +1,10 @@
-#include<stdio.h>  //Print in Reverse
+#include<stdio.h>  //Reverse a linked list
 #include<stdlib.h>
 struct node
 {
     int data;
     struct node *link;
 };
-
-int addnode(struct node **q,int num)
- {
-     struct node *temp,*r;
-     temp=*q;
-     r=malloc(sizeof(struct node));
-     r->data=num;
-     r->link=temp;
-     *q=r;
-     return 0;
- }
 
  int display(struct node *q)
  {
@@ -26,6 +15,49 @@ int addnode(struct node **q,int num)
       }
       return 0;
  }
+ int add_node(struct node **q,int num)
+ {
+     struct node *temp,*r;
+     temp=*q;
+     if(*q==NULL)
+     {
+         r=malloc(sizeof(struct node));
+         r->data=num;
+         r->link=NULL;
+         *q=r;
+         return 0;
+     }
+     else
+     {
+         temp=*q;
+         while(temp->link!=NULL)
+         {
+             temp=temp->link;
+         }
+         r=malloc(sizeof(struct node));
+         r->data=num;
+         r->link=NULL;
+         temp->link=r;
+         return 0;
+     }
+
+ }
+
+ int reverse(struct node **q)
+ {
+     struct node *temp,*r,*s;
+     r=NULL;s=NULL;temp=*q;
+     while(temp!=NULL)
+     {
+         s=r;
+         r=temp;
+         temp=temp->link;
+         r->link=s;
+     }
+     *q=r;
+     return 0;
+ }
+
  int main()
 {   struct node *p;
     p=NULL;
@@ -37,10 +69,17 @@ int addnode(struct node **q,int num)
     for(i=1;i<=n;i++)
      {   printf("enter data %d - ",i);
          scanf("%d",&num);
-         addnode(&p,num);
+         add_node(&p,num);
      }
-     printf("\nprinting the nodes in the reverse order.  ");
+
+     printf("\nbefore reversing the list   ");
+     display(p);
+
+     reverse(&p);
+
+      printf("\nafter reversing the list   ");
      display(p);
 
      return 0;
+
 }
